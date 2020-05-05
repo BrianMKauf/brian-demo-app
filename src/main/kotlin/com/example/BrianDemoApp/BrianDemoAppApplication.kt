bef06ea820fb7b80339a1c1bf6045c15177af5aa
Brian Kauf <Brian@thekaufs.com>
2020-05-05 14:16:48 -0500

    initlize repo with demo project

---
+++ src/main/kotlin/com/example/BrianDemoApp/BrianDemoAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.BrianDemoApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrianDemoAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrianDemoAppApplication>(*args)
+}
